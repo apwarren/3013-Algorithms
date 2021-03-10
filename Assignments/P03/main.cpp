ec3952216919066120daa64a60277ebe4364f10e
apwarren <allywarren7@gmail.com>
2021-03-10 13:22:57 -0600

    Comments have been added. Yay

--- Assignments/P03/main.cpp
+++ Assignments/P03/main.cpp
@@ -2,47 +2,51 @@
 *
 * Author:           Allyson Warren
 * Email:            allywarren7@gmail.com
-* Label:            P02
-* Title:            Processing in Linear Time
+* Label:            P03
+* Title:            Processing in Log Time
 * Course:           3013
 * Semester:         Spring 2021
 *
 * Description:
 *       This program takes words and definition from a json file and places
-*       the word and definition separately into a linked list. The list will
-*       be sorted due to the JsonFacade class. After the list is sorted in 
+*       the word and definition separately into a binary search tree. The tree will
+*       be sorted due to the JsonFacade class. The tree will ensure that it is balanced
+*       by finding the middle of the sorted list and branching off each middle section
+*       so that the search will maintain logn time. After the tree is formed in 
 *       O(n) time, the program will then prompt the user to type in a letter
 *       and show how many words were found that matched with the letter typed. 
-*       AFter each letter is typed, the computer will then continue to display how
-*       many similar words appear in the list and will also give ten suggested words
-*       from the list that it believes to be the word that is being typed by the 
+*       After each letter is typed, the computer will then continue to display how
+*       many similar words appear in the tree and will also give ten suggested words
+*       from the tree found first believed to be the word that is being typed by the 
 *       user. The program also displays how long it takes to find all similar words 
 *       and will display the time as well to the screen. As each letter is typed to 
-*       the screen from the user the search will become faster as the program 
-*       continues to look for similar words based on the last search it did. 
+*       the screen from the user the search will adjust accordingly and display any
+*       matches.
 * Usage:
 *       None for now
 *
 * Files:
 *       main.cpp         :  driver program
-*       dict_w_defs.json :  file containing words and definitions to be put in list
+*       dict_w_defs.json :  file containing words and definitions to be put in tree
 *       json.hpp         :  program to handle and retrieve dict_w_defs.json
 *       JsonFacade.hpp   :  program to condense json.hpp
 *       mygetch.hpp      :  program to allow getching and user input
-*       Timer.hpp        :  program to time how long it takes to find similar words
+*       timer.hpp        :  program to time how long it takes to find similar words
 *       trio.hpp         :  program to help with formatting screen ouput
+*       termcolor.hpp    :  program to allow for color change in the console
 *
 *******************************************************************************/
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include "JsonFacade.hpp"       // need to grab a copy from resources folder
+#include "JsonFacade.hpp"       
 #include "mygetch.hpp"
 #include "trio.hpp"
 #include "termcolor.hpp"
 #include <time.h>
 #include <chrono> 
-#include "timer.hpp"            // need to grab a copy from resources folder
+#include "timer.hpp"            
+
 using namespace std;
 
 /**
@@ -79,7 +83,7 @@ class wordNode
   public:
     /**
     * WordNode:
-    *    Default Constructor. Sets both string to NULL
+    *    Default Constructor. Sets both strings to NULL
     * Params:
     *    None
     *
@@ -94,8 +98,8 @@ class wordNode
 
     /**
     * WordNode(string, string):
-    *    Constructor that sets the two private string to
-    *     a specific strings.
+    *    Constructor that sets the two private strings to
+    *     a specific string.
     * Params:
     *    1 string   : word to be placed into list
     *    1 string   : definition of word to be placed in list
@@ -129,8 +133,8 @@ class wordNode
  * Node
  *
  * Description:
- *      A struct to create all nodes in the linked list.
- *      This function is only called in the List class.
+ *      A struct to create all nodes for the binary search tree
+ *      This function is only called in the BinarySearchTree class.
  *
  * Public Methods:
  *      - None
@@ -145,58 +149,58 @@ class wordNode
 struct Node
 {
     wordNode* Data; //Place to store any wordNode data in list
-    Node *left;     //Address of the left node of a parent node
+    Node *left;     //Address of the left node of a parent node (smaller value than parent)
     Node *right;    //Address of the right node (greater value than parent)
 };	
 
-Node* newNode(wordNode data);
 /**
- * List
+ * BinarySearchTree
  *
  * Description:
- *      A linked list class to hold the words and definition from
- *      the json file. The class also keeps track of both the front and
- *      back of the list as well as the last placed viewed in the list
- *      locations.  The list can compare letters of words and also 
- *      search in the list for any words that may be similar to a given
- *      word and count how many are in the list. 
+ *      A binary search tree class to hold the words and definition from
+ *      the json file. The class keeps track of the tree's root and
+ *      recursively traverses through to find a desired string. The tree
+ *      finds a desired string in log(n) time and stores the first 10 matching
+ *      strings found into a array of size 10 to be displayed when called in the 
+ *      Suggestions method. The tree can be filled by using the Fill method which 
+ *      takes in an instance of the JsonFacade class and puts the words from
+ *      the json file into the tree. 
  *      
  *
  * Public Methods:
- *      - List()
- *      - void Insert(string, string)
+ *      - BinarySearchTree()
  *      - void Fill(JsonFacade)
- *      - int Find(char)
- *      - string Suggestions(int)
+ *      - int Find(string)
+ *      - void Suggestions(int)
  * 
  * Private Methods:
- *      - None
+ *      - Node* Insert(vector<string> &, vector<string> &, int, int)
+ *      - int Find(Node*, string, int)
  *
  * Usage:
  *
- *  List J;
- *  J.Insert("word", "word2");
+ *  BinarySearchTree J;
  *  J.Fill(JsonF);
- *  J.Find('y');
+ *  J.Find("Hello");
  *  J.Suggestions(37);
+ *  Insert(vect, vect2, 5, 20);
+ *  Find(root, "Hello", 2);
  * 
  */
 class BinarySearchTree
 {
   private:
-    Node* Head;           //Points the front of the tree
-    Node* Current;        //Points to last placed viewed in list
-    int size;             //Size of the list
-    int index;            //Index of letter to compare
-    vector<string> keys;  //Vector of words in json object
-    vector<string> values;//Vector of defs in json object
+    Node* Head;                //Points the front of the tree
+    Node* Current;             //Points to last placed viewed in list
+    int size;                  //Size of the list
+    vector<string> keys;       //Vector of words in json object
+    vector<string> values;     //Vector of defs in json object
     string suggestedWords[10]; //Array of 10 suggested words found
-    bool Found;           //Flag to see if letter has been found
 
   public:
     /**
     * List
-    *    Default constructor for list class. Sets everything 
+    *    Default constructor for binary tree class. Sets everything 
     *    to nothing(NULL, 0 , "", etc.)
     * Params:
     *    None
@@ -212,43 +216,6 @@ class BinarySearchTree
     }
 
     /**
-    * Public: Insert
-    *    Function to directly place words and definitions into
-    *    the binary tree. Through recursion and binary searching, 
-    *    the tree is ensured that it is implemented balanced and is
-    *    allowed for logn search time. Each new node stores a word and 
-    *    definition into a wordNode and then its left and right child is
-    *    created and also called to be implemented. 
-    * Params:
-    *    vector<string>& : address of vector of words
-    *    vector<string>& : address of vector of definitions
-    *    int             : beginning of search index
-    *    int             : end of search index
-    *
-    * Returns:
-    *    Node*  : new Node added to the end of tree
-    */
-    Node* Insert(vector<string> &keys, vector<string> &values, int start, int end)
-    {
-      if(start > end) //At the start or end of word array
-      {
-        return NULL;
-      }
-
-        int middle = (start+end)/2;
-
-        Node* root = new Node;                                  //Create a new node/leaf
-        root->Data = new wordNode(keys[middle], values[middle]);//Store new data into leaf
-
-        root->left = Insert(keys, values, start,  middle - 1); //Create left side of branch
-
-        root->right = Insert(keys, values, middle + 1, end); //Create right side of branch
-
-        return root;
-       
-    } 
-
-    /**
     * Public: Fill
     *    This function fills the list with values given from a JsonFacade class
     *    that gets words and definitions from a json file. The method 
@@ -256,7 +223,7 @@ class BinarySearchTree
     *    the binary search tree. Insert is separate from fill as Insert handles placing
     *    items into the tree directlhy while Fill handles all json implementation. 
     * Params:
-    *    JsonFacade :   Class containing words and defintions to place into list
+    *    JsonFacade :   Class containing words and defintions to place into tree
     *
     * Returns:
     *    Void
@@ -272,13 +239,12 @@ class BinarySearchTree
 
     /**
     * Public: Find
-    *    This function compares all words in the list to a given letter
-    *    and keeps track of how many words contain the letter at that index.
-    *    If the letter is found then the count will start until that letter 
-    *    no longer matches. The list is sorted so it works. The location of 
-    *    the letter is also taken into consideration; for instance: If it has 
-    *    already found all 'W' words then it can compare the next letter in the 
-    *    word to minimize the list. 
+    *    This function compares all words in the list to the current typed word
+    *    and only compares a substring with the same length from the dictionary word.
+    *    This function hides all the innerworkings of the tree and its nodes. It allows 
+    *    for anything outside of the class to compare a string without showing and relying on
+    *    it to know how its achieved. This method calls the private Find function to actually
+    *    begin the search and recursively return how many matches were found in the tree. 
     * Params:
     *    string   :   typed word to compare with tree's node word
     *
@@ -288,6 +254,7 @@ class BinarySearchTree
     int Find(string word)
     {
         return Find(Head, word, 0); //Call private method to traverse through tree
+                                    //No words have been found yet
     }
 
   
@@ -296,61 +263,139 @@ class BinarySearchTree
     *    This function returns words that closely match the word
     *    typed by the user. If there are more than ten words that
     *    match the input, then the function prints the first
-    *    ten matching words in the list unless there are less.
+    *    ten matching words in the tree unless there are less.
     *    Otherwise, it will return all matching words if less 
-    *    than 10 only. 
+    *    than 10 only. The 10 words printed are the first 10
+    *    found in the tree. The first match printed will appear
+    *    red while the rest will show as default white on the console.
     * Params:
     *    int    :   # of words found to match input
     *
     * Returns:
-    *    string :   first 10 or less similar words to input
+    *    void
     */
     void Suggestions(int total)
     {
-        string suggests = "";   //All words that currently match
-        int words = 0;          //Number of words added to string
+        int words = 0;          //Number of words already printed
+
         //Continue until 10 words found, no more matches, or end of list
         while(words < 10 && words < total)
         {
-            if(words == 0)//First suggested word is printed in red
+            if(words == 0)      //First suggested word is printed in red
             {
               cout << termcolor::red;
             }
+
             cout << suggestedWords[words] + "  ";  //Print word to screen
             cout << termcolor::reset;              //Print in normal color
+
             words++;                               //Increment words to suggest
         }
 
     }
-  private:
+
+  private:  
+    /**
+    * Private: Insert
+    *    Function to directly place words and definitions into
+    *    the binary tree. Through recursion and binary searching, 
+    *    the tree is ensured that it is balanced when implemented and
+    *    allows for logn search time. Each new node stores a word and 
+    *    definition into a wordNode and then its left and right child is
+    *    created and also called to be implemented. This method is only called
+    *    in this class.
+    * Params:
+    *    vector<string>& : address of vector of words
+    *    vector<string>& : address of vector of definitions
+    *    int             : beginning of search index
+    *    int             : end of search index
+    *
+    * Returns:
+    *    Node*  : new Node added to the end of tree
+    */
+    Node* Insert(vector<string> &keys, vector<string> &values, int start, int end)
+    {
+        if(start > end) //At the start or end of vector search
+        {
+            return NULL;
+        }
+
+        int middle = (start+end)/2; //Get middle to binary traverse for balanced tree
+
+        Node* root = new Node;                                  //Create a new node/leaf
+        root->Data = new wordNode(keys[middle], values[middle]);//Store new data into leaf
+
+
+        root->left = Insert(keys, values, start,  middle - 1);  //Create left side of branch
+        root->right = Insert(keys, values, middle + 1, end);    //Create right side of branch
+
+        return root;
+       
+    } 
+
+    /**
+    * Private: Find
+    *    This function compares all words in the list to the current typed word
+    *    and only compares a substring with the same length from the dictionary word.
+    *    The strings are compared using the compare method in the string library in 
+    *    which it total the ascii value of the typed word with the matching substring
+    *    of the tree's word. It then subtracts the two values. If the strings match
+    *    then the value will be 0 and the tree branches off both left and right to 
+    *    continue to see if more matches are nearby. If the subtracted value is a 
+    *    negative number then the dictionary word is after the desired word in the
+    *    alphabet and only the left side of the node will be traversed as none of the 
+    *    right side can match. Similarly, if the subtraction is greater than 0, the
+    *    substring is before the typed word in the alphabet and only the right side of
+    *    the current node is traversed. This allows for log(n) search time where not 
+    *    all of the tree has to be searched to find all matches. The method also fills
+    *    an array of size 10 with the first ten matching words fourd in the tree to be
+    *    printed later in the Suggestions function. The function returns the total 
+    *    number of matches found in the tree through recursion.
+    * Params:
+    *    Node*    :   current root node to compare with string
+    *    string   :   typed word to compare with tree's node word
+    *    int      :   number of words placed into array of matches
+    *
+    * Returns:
+    *     int   :   Total number of similar words in list
+    */
     int Find(Node* root, string word, int matches)
     {
-        int SuggestedSum = matches; //Number of words added to suggestions
-        if(root == NULL)//AT the end of tree branch, No more leaves
+        int SuggestedSum = matches; //Number of words already added to suggestions
+
+        if(root == NULL)//AT the end of tree branch, Base case
         {
-          return 0;
+          return 0;     //Go back to previous call
         }
-        string DictWord;    //Store word from tree to compare
+
+        string DictWord;                   //Store word from tree to compare
         
         DictWord = root->Data->getWord();  //Get each dictionary word
 
-        int comparison = DictWord.compare(0, word.size(), word);//Compare ascii values of words
+        int comparison = DictWord.compare(0, word.size(), word);//Get difference of ascii values from strings
 
-        if(comparison == 0)//Word matches substring
+
+        if(comparison == 0)                         //Word matches substring
         {       
-          if(SuggestedSum < 10) 
+          if(SuggestedSum < 10)     //Place first 10 matches into array
           {
-            suggestedWords[SuggestedSum] = DictWord;  //Add word to suggested words
-            SuggestedSum++;                    //Word has been added to vector/ size increments
+            suggestedWords[SuggestedSum] = DictWord;//Add word to suggested words
+            SuggestedSum++;                         //Word has been added to vector/ size increments
           }
+                                                    //Continue looking for matches
           return 1 + Find(root->right, word, SuggestedSum) + Find(root->left, word, SuggestedSum); //save # of matches
         }
-        else if(comparison < 0)//Word is after root word
+
+
+        else if(comparison < 0)                     //Word is after root word
         {
+                                                    //Only search right side of branch
           return Find(root->right, word, SuggestedSum);
         }
-        else  //Word is before root word in alphabet
+
+        else                                        //Word is before root word in alphabet
         {
+                                                    //Only search left side of branch
           return Find(root->left, word, SuggestedSum);
         }
     }
@@ -371,11 +416,11 @@ int main()
     int total;                          //total matches found in list
     int wordSize = 0;                   //Size of typed word
 
-    WordDefs.Fill(J);    //Fill tree w/ dictionary words
+    WordDefs.Fill(J);                   //Fill tree w/ dictionary words
     
     cout << setprecision(3) << "input: ";//Prompt user to type & set decimals
 
-                                        //Continue until user presses enter
+                                         //Continue until user presses enter
     while ((int)(letter = getch()) != 10) 
     {
         wordSize++;                    //User has pressed a key
@@ -403,8 +448,8 @@ int main()
         cout << termcolor::red << word; //Word is printed red
         cout << termcolor::reset <<'\n';//Reset coloring to normal
 
-        T.Start();                    //Begin timer to look for matches
-        total = WordDefs.Find(word);  //Total is # of all matches found
+        T.Start();                      //Begin timer to look for matches
+        total = WordDefs.Find(word);    //Total is # of all matches found
 
         cout << termcolor::red << total;//Typed word is shown in red
         cout << termcolor::reset;       //Go back to normal ouput
@@ -415,8 +460,8 @@ int main()
         millsec = T.MilliSeconds();     //Set to number of milliseconds
         
         cout << setw(4) << sec << " seconds" << endl; //Diplay time to find matches
-        WordDefs.Suggestions(total);
-        io << endWord; //Display suggested matches
+        WordDefs.Suggestions(total);                  //Display suggested matches
+        io << endWord;                                //Place cursor after typed word
         
     }
 }
